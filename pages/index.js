c8e4e1e35500227fd99dda8c37816611622d255c
Karim Benkiran <karim.benkiran@dprgroup.ma>
2026-01-13 14:09:35 +0100

    Create Home component for main page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "50px", fontFamily: "sans-serif" }}>
+      <h1>DPR AI Studio</h1>
+      <p>Le déploiement est réussi ! 🚀</p>
+    </div>
+  );
+}
