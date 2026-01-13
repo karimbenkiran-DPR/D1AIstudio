599d0914f30808c1ec3b1fc2751dd8d66eaca367
Karim Benkiran <karim.benkiran@dprgroup.ma>
2026-01-13 15:30:46 +0100

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "50px", fontFamily: "sans-serif" }}>
-      <h1>DPR AI Studio</h1>
-      <p>Le déploiement est réussi ! 🚀</p>
-    </div>
-  );
-}
